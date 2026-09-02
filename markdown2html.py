03a7444ea7033c4b0d88bf1990407332654e0f97
Michael Cuffaro <consulting@michaelcuffaro.com>
2026-09-02 14:43:31 -0400

    remove delay

--- markdown2html.py
+++ markdown2html.py
@@ -69,7 +69,6 @@ def main():
         html = is_tooltip.sub(
             'data-bs-toggle="tooltip" '
             'data-bs-trigger="hover" '
-            'data-bs-delay="1000" '
             'data-bs-html="true" '
             'title="',
             html
