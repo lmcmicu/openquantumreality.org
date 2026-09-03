6326a304e6e9206dd63ecbd29ee34bc89d0233c8
Michael Cuffaro <consulting@michaelcuffaro.com>
2026-09-02 21:40:52 -0400

    tweak

--- markdown2html.py
+++ markdown2html.py
@@ -68,8 +68,6 @@ def main():
         html = is_th.sub('<th class="font-monospace fst-italic" ', html)
         html = is_tooltip.sub(
             'data-bs-toggle="tooltip" '
-            'data-bs-trigger="hover" '
-            'data-bs-placement="auto" '
             'data-bs-html="true" '
             'title="',
             html
